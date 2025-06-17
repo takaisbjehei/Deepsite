b8b3bd22318a1efc89bb74117fbad67b31988509
enzo <eesteves@hotmail.fr>
2025-06-17 07:08:21 -0400

    add log to check free requests

--- app/api/ask-ai/route.ts
+++ app/api/ask-ai/route.ts
@@ -68,7 +68,7 @@ export async function POST(request: NextRequest) {
     authHeaders.get("x-forwarded-for")?.split(",")[0].trim() ||
     authHeaders.get("x-real-ip") ||
     "0.0.0.0";
-  console.log(authHeaders);
+  console.log(request.headers);
 
   if (!token) {
     ipAddresses.set(ip, (ipAddresses.get(ip) || 0) + 1);
